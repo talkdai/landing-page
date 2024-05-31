f76c46010cf7147e1da37b08a3a04f29aa76f3e7
Juan Diego Degli Esposte Rosa <juanddegliesposte@gmail.com>
2024-05-31 10:57:45 -0300

    Adding custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        'primary-purple':'#9A59FF',
+        'primary-gray': '#B1C2BE',
+        'dark-blue': '#001F26',
+        'primary-black': '#191919',
       },
     },
   },
