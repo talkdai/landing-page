5a3c7a50fa9f96cf6edf4001f4099531346a4cfb
Juan Diego Degli Esposte Rosa <juanddegliesposte@gmail.com>
2024-06-10 08:50:01 -0300

    repositioning and adding card components

--- src/components/SectionHero.tsx
+++ src/components/SectionHero.tsx
@@ -1,33 +1,77 @@
-import { Container } from "./container";
+import { Cards } from "./Card";
+import { Container } from "./Container";
 
 export function SectionHero() {
-    return(
-        <section className="pt-5 w-full h-[704px] bg-black bg-no-repeat bg-cover bg-center talkdai-hero">
-            <Container>
-                <div className=" flex-1 max-w-[700px] mx-auto px-4 text-center">
-                    <h1 className=" text-4xl font-bold">
-                        <span className=" text-primary-gray">Talkd is an open-source software that enables you to </span>
-                        <span className=" text-primary-purple">easily deploy any LLM available through LangChain.</span>
-                    </h1>
-                        <p className=" justify-items-center text-xl mx-auto px-4 max-w-[600px] text-primary-gray p-5">We’re currently in beta, working on making it as easy as possible to deploy your own LLM. </p>
+  return (
+    <section className="w-full lg:max-w-[1440px] h-auto bg-black bg-no-repeat bg-cover bg-center talkdai-hero text-white">
+      <Container>
+        <div className=" w-full max-w-[954px] mx-auto text-center px-4">
+          <div className="mb-24">
+            <h1
+              className=" font-semibold md:text-[46px]"
+              style={{ lineHeight: 1.2 }}
+            >
+              <span className=" text-primary-gray">
+                talkd is an open-source software that enables you to{" "}
+              </span>
+              <span className=" text-primary-purple">
+                easily deploy any LLM available through LangChain.
+              </span>
+            </h1>
+            <p
+              className="text-[26px] mx-auto max-w-[675px] text-primary-gray mt-6"
+              style={{ lineHeight: 1.2 }}
+            >
+              We’re currently in beta, working on making it as easy as possible
+              to deploy your own LLM.{" "}
+            </p>
+          </div>
 
-                        <div className="flex justify-center gap-6 mt-8">
-                            <button className="p-4 border border-gray-500 rounded-md max-w-[300px] text-left">
-                                <h2 className="text-2xl font-bold text-white">Dialog</h2>
-                                    <p className="text-primary-gray mt-2">
-                                        An extensible LLM API that enables you to easily deploy any LLM.
-                                    </p>
-                            </button>
-                            <button className="p-4 border border-gray-500 rounded-md max-w-[300px] text-left">
-                                <h2 className="text-2xl font-bold text-white">Whatsapp Plugin</h2>
-                                    <p className="text-primary-gray mt-2">
-                                    Our first plugin, enables users to launch Dialog with WhatsApp.
-                                    </p>
-                            </button>
+          <div className="w-full max-w-[997px] h-auto flex flex-col md:flex-row justify-center gap-6 font-normal font-ubuntu-mono">
+            <a
+              href="https://github.com/talkdai/dialog"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-4 border-2 border-gray-500 rounded-md gap-6 text-left flex flex-col items-start"
+            >
+              <h2 className="text-2xl font-semibold text-primary-gray font-ubuntu-mono">
+                Dialog
+              </h2>
+              <p className="w-[362px] text-primary-gray md:text-2xl mt-2">
+                An extensible LLM API that enables you to easily deploy any LLM.
+              </p>
+            </a>
+            <a
+              href="https://github.com/talkdai/dialog-whatsapp"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-4 border-2 border-gray-500 rounded-md gap-6 text-left flex flex-col items-start"
+            >
+              <h2 className="text-2xl font-bold text-primary-gray">
+                Whatsapp Plugin
+              </h2>
+              <p className="w-[362px] text-primary-gray md:text-2xl mt-2">
+                Our first plugin, enables users to launch Dialog with WhatsApp.
+              </p>
+            </a>
+          </div>
 
-                        </div>
-                </div>
-            </Container>
-        </section>
-    )
+          <div className=" w-full max-w-[954px] mx-auto text-center px-4 text-primary-gray mt-32">
+            <h1
+              className=" font-semibold md:text-[46px]"
+              style={{ lineHeight: 1.2 }}
+            >
+              What our customers say about us
+            </h1>
+          </div>
+            <div className="flex flex-col mt-24 justify-center gap-6 md:flex-row md:space-x-6 mb-16">
+                <Cards/>
+                <Cards/>
+                <Cards/>
+          </div>
+
+        </div>
+      </Container>
+    </section>
+  );
 }
