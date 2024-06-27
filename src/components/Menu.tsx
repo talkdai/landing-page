0ca31df766c5ece310e685bfbb3dd9fb25e7c422
Juan Diego Degli Esposte Rosa <juanddegliesposte@gmail.com>
2024-06-27 15:45:25 -0300

    adding route and styling selected page menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,16 @@
+"use client"; 
+
+import { usePathname } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
 
 export const Menu = () => {
-  const [selected, setSelected] = useState('Home');
+  const pathname = usePathname();
+  const [selected, setSelected] = useState( pathname.toLowerCase());
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const menuItems = [
-    {page: 'Home', link: '#'},
-    {page: 'About', link: '#'},
+    { page: 'Home', link: '/' }, 
+    { page: 'About', link: '/About' }, 
   ];
 
   // Close the mobile menu when the screen width exceeds 1000px
@@ -19,7 +23,6 @@ export const Menu = () => {
 
     window.addEventListener('resize', handleResize);
 
-    // Cleanup the event listener on component unmount
     return () => {
       window.removeEventListener('resize', handleResize);
     };
@@ -31,14 +34,16 @@ export const Menu = () => {
         {menuItems.map(item => (
           <div
             key={item.page}
-            className={`items-center flex gap-2 pl-2 pr-2 py-2 md:pl-4 md:pr-4 md:py-3 md:max-w-[340px] md:h-[40px] menu-item ${selected === item.page ? 'selected' : ''}`}
-            onClick={() => setSelected(item.page)}
+            className={`items-center flex gap-2 pl-2 pr-2 py-2 md:pl-4 md:pr-4 md:py-3 md:max-w-[340px] md:h-[40px] menu-item ${
+              selected === item.link.toLowerCase() ? 'selected' : '' 
+            }`}
+            onClick={() => setSelected(item.link.toLowerCase())}
           >
             <a href={item.link}>
               {item.page}
             </a>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-              <path d="M12.9602 15.7131L18.9702 9.70309L17.5572 8.28809L12.9602 12.8881L8.36419 8.28809L6.9502 9.70209L12.9602 15.7131Z" fill={`${selected === item.page ? '#9A59FF' : '#fff'}`}/>
+              <path d="M12.9602 15.7131L18.9702 9.70309L17.5572 8.28809L12.9602 12.8881L8.36419 8.28809L6.9502 9.70209L12.9602 15.7131Z" fill={pathname.toLowerCase() === item.link.toLowerCase() ? '#9A59FF' : '#fff'}/>
             </svg>
           </div>
         ))}
